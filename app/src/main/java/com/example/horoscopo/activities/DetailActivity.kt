65bbe6b50d9faa1a9c7f19ce4d0efb7c0914eee9
Mansour <theboom.kajas@gmail.com>
2024-11-04 17:37:51 +0100

    Changed the themes file to show the ActionBar. Added a new menu for DetailActivity. Implemented the menu in DetailActivity. Enabled the ActionBar back button. Added some icons for the menu and the strings.

--- app/src/main/java/com/example/horoscopo/activities/DetailActivity.kt
+++ app/src/main/java/com/example/horoscopo/activities/DetailActivity.kt
@@ -1,13 +1,13 @@
 package com.example.horoscopo.activities
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.horoscopo.R
 import com.example.horoscopo.data.Horoscope
 import com.example.horoscopo.data.HoroscopeProvider
@@ -25,10 +25,48 @@ class DetailActivity : AppCompatActivity() {
 
         horoscope = HoroscopeProvider.findById(id)
 
+        supportActionBar?.title = getString(horoscope.name)
+        supportActionBar?.subtitle = getString(horoscope.dates)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
         findViewById<TextView>(R.id.tv).setText(horoscope.name)
         findViewById<ImageView>(R.id.iv).setImageResource(horoscope.image)
         findViewById<Button>(R.id.b).setOnClickListener {
             finish()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_detail_activity, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                println("Menu home")
+                finish()
+                return true
+            }
+            R.id.menu_favorite -> {
+                println("Menu favorito")
+                return true
+            }
+            R.id.menu_share -> {
+                println("Menu compartir")
+                return true
+            }
+            else -> {
+                return super.onOptionsItemSelected(item)
+            }
+        }
+    }
+
+    override fun onBackPressed() {
+        if (horoscope.id == "aries") {
+            Toast.makeText(this, "No puedes volver", Toast.LENGTH_LONG).show()
+        } else {
+            super.onBackPressed()
         }
     }
 }
